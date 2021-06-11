fffe516366651e9438d9112457f7561f170abb1f
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-10 20:06:09 -0400

    mvp

--- api/model.js
+++ api/model.js
@@ -1,44 +1,59 @@
 const db = require('../data/db-config')
-
 async function getRecipeById(recipe_id) {
-    const rows = await db.select('rp.*', 'st.step_id', 'st.step_number', 'st.step_instructions', 'ing.*', 'si.ingredient_quantity')
-    .from('recipes as rp')
-    .join('steps as st', 'rp.recipe_id', '=', 'st.recipe_id')
-    .join('steps_ingredients as si', 'st.step_id', '=', 'si.step_id')
-    .join('ingredients as ing', 'si.ingredient_id', '=', 'ing.ingredient_id')
-    .where('rp.recipe_id', recipe_id)
-
-    const result = {
-        recipe_id: rows[0].recipe_id,
-        recipe_name: rows[0].recipe_name,
-        created_at: rows[0].created_at,
-        steps: []
-    }
-    rows.forEach(row => {
-        if(row.step_id) {
-            result.steps.push({
-                step_id: row.step_id,
-                step_number: row.step_number,
-                step_instructions: row.step_instructions,
-                ingredients: []
-            })
+    const data = await db('recipes as rp')
+        .select('rp.*', 'st.step_id', 'st.step_number', 'st.step_instructions', 'ing.*', 'si.ingredient_quantity')
+        .join('steps as st', 'rp.recipe_id', 'st.recipe_id')
+        .join('steps_ingredients as si', 'st.step_id', 'si.step_id')
+        .join('ingredients as ing', 'si.ingredient_id', 'ing.ingredient_id')
+        .where('rp.recipe_id', recipe_id)
+    //select rp.*, st.*, ing.*,si.ingredient_quantity FROM recipes as rp
+    // join steps as st on rp.recipe_id = st.recipe_id
+    // join steps_ingredients as si on st.step_id = si.step_id
+    // join ingredients as ing on si.ingredient_id = ing.ingredient_id
+    // const result = {
+    //     recipe_id: rows[0].recipe_id,
+    //     recipe_name: rows[0].recipe_name,
+    //     created_at: rows[0].created_at,
+    //     steps: []
+    // }
+    // rows.forEach(row => {
+    //     if(row.step_id) {
+    //         result.steps.push({
+    //             step_id: row.step_id,
+    //             step_number: row.step_number,
+    //             step_instructions: row.step_instructions,
+    //             ingredients: []
+    //         })
+    //     }
+    // })
+    // rows.forEach(row => {
+    //     if(row.ingredients_id) {
+    //         result.steps.push({
+    //             ingredients: 
+    //             {ingredient_id: row.ingredient_id,
+    //             ingredient_name: row.ingredient_name,
+    //             quantity: row.quantity}
+    //         })
+    //     }
+    // })
+    // return result
+    const steps = data.map(x => {
+        return {
+          step_id: x.step_id,
+          step_number: x.step_number,
+          step_instructions: x.step_instructions,
+          ingredients: x.ingredient_name,
+          quantity: x.ingredient_quantity
         }
-    })
-    rows.forEach(row => {
-        if(row.ingredients) {
-            result.steps.ingredients.push({
-                ingredient_id: row.ingredient_id,
-                ingredient_name: row.ingredient_name,
-                quantity: row.quantity
-            })
-        }
-    })
-    return result
-  
+      })
+      const transformedData = {
+        recipe_id: data[0].recipe_id,
+        recipe_name: data[0].recipe_name,
+        created_at: data[0].created_at,
+        steps: steps[0].step_id == null ? [] : steps
+      }
+      return transformedData
 }
-
-
-
 module.exports = {
     getRecipeById
 }
